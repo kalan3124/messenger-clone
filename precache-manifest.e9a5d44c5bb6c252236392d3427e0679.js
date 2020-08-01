32e7f22b95e2b6f6a145c426c68a72d39ec4851d
Imalsha kalansooriya <30498171+kalan3124@users.noreply.github.com>
2020-08-01 21:18:51 +0530

    Updates

---
+++ precache-manifest.e9a5d44c5bb6c252236392d3427e0679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9b612d4e1c0b8cdc8742e71a7189b1",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "afaa7de27865ca1431f1",
+    "url": "/messenger-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06a0b5db4b91b19903c5",
+    "url": "/messenger-clone/static/js/2.27661b97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/messenger-clone/static/js/2.27661b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afaa7de27865ca1431f1",
+    "url": "/messenger-clone/static/js/main.c958c414.chunk.js"
+  },
+  {
+    "revision": "ec85557f6cfdb048b3bb",
+    "url": "/messenger-clone/static/js/runtime-main.76eefcd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/messenger-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
