5e8a0e91363d359e2c5149ab5bc0ead5e0b526cc
Imalsha kalansooriya <30498171+kalan3124@users.noreply.github.com>
2020-08-01 22:52:41 +0530

    Updates

---
+++ precache-manifest.e225de93c91e4bc08098272ab3ebc890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06c1d814eb9da9eadde88dc5fd17cdd",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "c08ec19e7898e883e200",
+    "url": "/messenger-clone/static/css/main.5ea7254b.chunk.css"
+  },
+  {
+    "revision": "c2f8aaca886ecbcdfea2",
+    "url": "/messenger-clone/static/js/2.1a47f5e9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger-clone/static/js/2.1a47f5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08ec19e7898e883e200",
+    "url": "/messenger-clone/static/js/main.a58c68fd.chunk.js"
+  },
+  {
+    "revision": "ec85557f6cfdb048b3bb",
+    "url": "/messenger-clone/static/js/runtime-main.76eefcd8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/messenger-clone/precache-manifest.9c791bc37ff236e9619aae6a6580c1dc.js"
+  "/messenger-clone/precache-manifest.e225de93c91e4bc08098272ab3ebc890.js"
 );
 
 self.addEventListener('message', (event) => {
